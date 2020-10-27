153915a2044e7d51712b751eac22abdda3a9cfc8
Kevin Herrera <1herrerake@gmail.com>
2020-10-27 00:09:48 -0500

    initial commit. changed website meta data title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `iight Bet`,
+    description: `The Social Betting App Made by us for us`,
+    author: `@herrerake`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
