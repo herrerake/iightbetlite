a5a1cec4f79b4f10e375ae06569fc0974c124a9c
Kevin Herrera <1herrerake@gmail.com>
2020-11-12 19:07:08 -0600

    ready for week 10

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -5,15 +5,15 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="NFL Week 9 Sheet" />
-    <h1>NFL Week 9 Sheet</h1>
-    <h3>Pot this week is $350</h3>
-    <p>Check back 30min before TNF kickoff to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQkw5lgOradN9_9u9bnIGsoVqJTIbWZShCqL49DW2mwQcXH_PkVQqcxrGuGTtU-qGJicmr5r2687skn/pubhtml?gid=1263257633&amp;single=true&amp;widget=true&amp;headers=false"
+    <SEO title="NFL Week 10 Sheet" />
+    <h1>NFL Week 10 Sheet</h1>
+    <h3>Pot this week is $325</h3>
+    {/* <p>Check back 30min before TNF kickoff to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTvGj7_F0ofci-mpEBEpyZDIWWb3iEigt8zfXu0LWjQJIgEM99KYWVXRZ4ZuyOonNDWUzTCP2NKurbU/pubhtml?gid=417030378&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
