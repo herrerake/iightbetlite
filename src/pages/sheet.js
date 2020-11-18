bcd99ed9a3913270c082e5ad9bc186cd8d33b637
Kevin Herrera <1herrerake@gmail.com>
2020-11-17 20:33:04 -0600

    adjusted sheet

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 11 Sheet" />
-    <h1>NFL Week 10 Sheet</h1>
-    <h3>Pot this week is $325</h3>
+    <h1>NFL Week 11 Sheet</h1>
+    {/* <h3>Pot this week is $325</h3> */}
     <p>Check back 30min before TNF kickoff to see the sheet.</p>
     {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTvGj7_F0ofci-mpEBEpyZDIWWb3iEigt8zfXu0LWjQJIgEM99KYWVXRZ4ZuyOonNDWUzTCP2NKurbU/pubhtml?gid=417030378&amp;single=true&amp;widget=true&amp;headers=false"
