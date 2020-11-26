63a092a1e7b02b7085b9fc5c867b70bcf9111559
Kevin Herrera <1herrerake@gmail.com>
2020-11-26 10:50:47 -0600

    game day ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,13 +7,13 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 12 Sheet" />
     <h1>NFL Week 12 Sheet</h1>
-    {/* <h3>Pot this week is $200</h3> */}
-    <p>Check back 30min before TNF kickoff to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQCgZwJcq6EQ6fWtYkYfHkL2Sj8Yr1qJe9bF-XEd1rcIMlqvaMglGlAv8MaxIC-JkN6qg-BSaHHXjco/pubhtml?gid=509084813&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $250</h3>
+    {/* <p>Check back 30min before TNF kickoff to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQAyIXsNx4jmUOmYRpdxXZxSomf1aYobTpaPAVker_UAnl7N7NmEi-I2wbuEdQhZNKiMwRs8RbT4lbt/pubhtml?gid=147846881&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
