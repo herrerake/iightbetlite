94fa0628216dd1f7df3f4107b9eb090d263b2f27
Kevin Herrera <1herrerake@gmail.com>
2020-12-10 19:21:21 -0600

    week 14 sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ import SEO from "../components/seo"
 const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 14 Sheet" />
-    <h1>NFL Week 12 Sheet</h1>
-    {/* <h3>Pot this week is $250</h3> */}
-    <p>Check back 30min before TNF kickoff to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQAyIXsNx4jmUOmYRpdxXZxSomf1aYobTpaPAVker_UAnl7N7NmEi-I2wbuEdQhZNKiMwRs8RbT4lbt/pubhtml?gid=147846881&amp;single=true&amp;widget=true&amp;headers=false"
+    <h1>NFL Week 14 Sheet</h1>
+    <h3>Pot this week is $150</h3>
+    {/* <p>Check back 30min before TNF kickoff to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTCVs5w7PNwWUq2NS831zbySxBM1PNLpKHnzmi5mm8hnEUT69J8-9oe62JsguhpHEJrCXPvNRomDh-K/pubhtml?gid=1813967893&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
