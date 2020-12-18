2072c7a60963e9a21ffdd98dd78da2dfe29a157e
Kevin Herrera <1herrerake@gmail.com>
2020-12-17 20:16:51 -0600

    edit

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,7 +7,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 15 Sheet" />
     <h1>NFL Week 15 Sheet</h1>
-    <h3>Pot this week is $150</h3>
+    <h3>Pot this week is $275</h3>
     {/* <p>Check back 30min before TNF kickoff to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRaJy0ekH-m4Sud6JCyA6IaeBMyO3ZsS_CLxB9aaRltjkefN619zN691gKOWBAn_1ygvRbwudgc73TS/pubhtml?gid=592686307&amp;single=true&amp;widget=true&amp;headers=false"
