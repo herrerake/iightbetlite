26be2f94e3c8aa7e9db3ed462c192fef7e5547ed
Kevin Herrera <1herrerake@gmail.com>
2020-12-25 16:27:44 -0500

    week 16 ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,14 +7,15 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 16 Sheet" />
     <h1>NFL Week 16 Sheet</h1>
-    {/* <h3>Pot this week is $275</h3> */}
-    <p>Check back 30min before Christmas Day kickoff to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRaJy0ekH-m4Sud6JCyA6IaeBMyO3ZsS_CLxB9aaRltjkefN619zN691gKOWBAn_1ygvRbwudgc73TS/pubhtml?gid=592686307&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $100</h3>
+    {/* <p>Check back 30min before Christmas Day kickoff to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS7iHpUgee0QviCs5uHmutpRIveQ2BHlQvMPWxh81WWhfmhkqHSS1VsSG9tnIBqveMnYVEc-fdzIaZg/pubhtml?gid=1553934676&single=true"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
 export default SheetPage
+
