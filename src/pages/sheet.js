256932723cc4f7edc9853dee60cd8c7e0058dfd9
Kevin Herrera <1herrerake@gmail.com>
2021-01-03 23:51:17 -0600

    sheet 17

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,15 +7,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Week 17 Sheet" />
     <h1>NFL Week 17 Sheet</h1>
-    {/* <h3>Pot this week is $100</h3> */}
-    <p>Check back 30min before Sunday kickoff to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS7iHpUgee0QviCs5uHmutpRIveQ2BHlQvMPWxh81WWhfmhkqHSS1VsSG9tnIBqveMnYVEc-fdzIaZg/pubhtml?gid=1553934676&single=true"
+    <h3>Pot this week is $100</h3>
+    {/* <p>Check back 30min before Sunday kickoff to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQKRCQwRVWu02-GmWgYScE3oSsdtxBnP_OqV-Fhp6CDSewdMMxc-tzkhd5eQeK1m8FGL0y_e8bvpMXW/pubhtml?gid=720873442&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
 export default SheetPage
-
