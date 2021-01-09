27b83f0249390bec093fb287694a2bae466cf501
Kevin Herrera <1herrerake@gmail.com>
2021-01-09 12:36:48 -0600

    update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,7 +7,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Wild Card Sheet" />
     <h1>NFL Wild Card Sheet</h1>
-    <h3>Pot this week is $225</h3>
+    <h3>Pot this week is $250</h3>
     {/* <p>Check back 30min before the first Saturday kickoff to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSSIMMd5ZCeSqt3NMiq0f_sggHwvw8LG8rS2g4DiTcfm9TV834_mAVTggFgTWnOUnRETFdE2WsgCXfH/pubhtml?gid=2125143015&amp;single=true&amp;widget=true&amp;headers=false"
