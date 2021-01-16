356411d31a76655dc02678b30ec6cd2ed6097d06
Kevin Herrera <1herrerake@gmail.com>
2021-01-16 15:39:15 -0600

    update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,7 +7,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="NFL Divisional Round Sheet" />
     <h1>NFL Divisional Round Sheet</h1>
-    <h3>Pot this week is $250</h3>
+    <h3>Pot this week is $275</h3>
     {/* <p>Check back 30min before the first Saturday kickoff to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSHSW8-b6Ekuwuv1TYzYvVF58rfMYT8VaPJYswOqIfiYViNKhqjQy_zYAGZaaOFETLvfesM2Y_ZEfAF/pubhtml?gid=77793139&amp;single=true&amp;widget=true&amp;headers=false"
