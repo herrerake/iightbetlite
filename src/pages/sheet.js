f687506edfcb2cd1cbc2be3a59914358ef88d05a
Kevin Herrera <1herrerake@gmail.com>
2021-01-16 15:34:15 -0600

    sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -5,12 +5,12 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="NFL Wild Card Sheet" />
-    <h1>NFL Wild Card Sheet</h1>
+    <SEO title="NFL Divisional Round Sheet" />
+    <h1>NFL Divisional Round Sheet</h1>
     <h3>Pot this week is $250</h3>
     {/* <p>Check back 30min before the first Saturday kickoff to see the sheet.</p> */}
     <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSSIMMd5ZCeSqt3NMiq0f_sggHwvw8LG8rS2g4DiTcfm9TV834_mAVTggFgTWnOUnRETFdE2WsgCXfH/pubhtml?gid=2125143015&amp;single=true&amp;widget=true&amp;headers=false"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSHSW8-b6Ekuwuv1TYzYvVF58rfMYT8VaPJYswOqIfiYViNKhqjQy_zYAGZaaOFETLvfesM2Y_ZEfAF/pubhtml?gid=77793139&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
     ></iframe>
