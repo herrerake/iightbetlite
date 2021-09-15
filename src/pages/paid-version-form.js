02553c159db70d338a70c81a7717bcf3ed699d7c
Kevin Herrera <1herrerake@gmail.com>
2021-09-14 22:19:49 -0500

    week 2 ready

--- src/pages/paid-version-form.js
+++ src/pages/paid-version-form.js
@@ -8,13 +8,13 @@ import { Container, Row, Col } from "react-bootstrap"
 
 const FormPage = () => (
   <Layout>
-    <SEO title="NFL Week 1 Form" />
+    <SEO title="NFL Week 2 Form" />
     <Container>
       <Row>
         <Col>
           <h1>Weekly Pool</h1>
           <p>Winner take all</p>
-          <p>Picks are due by Thursday @ 5pm EST</p>
+          <p>Picks are due by Thursday @ 7pm EST</p>
           <p>
             By submitting picks to this pool you agree to follow the rules laid
             out on <Link to="/rules">this page</Link>
@@ -24,13 +24,13 @@ const FormPage = () => (
             know your nickname.
           </p>
           <p>
-            NOTE: 10% Admin fee will be subtracted from winners earnings
+            NOTE: 5% Admin fee will be subtracted from winners earnings
           </p>
         </Col>
       </Row>
     </Container>
     <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSd5KljoTHf9ufhPyHnd2nV3blCcqktyZu4ueqP83b-oNLlXFw/viewform?embedded=true"
+      src="https://docs.google.com/forms/d/e/1FAIpQLScm9u9kRxrLOjHS4PnnratMKJPfVHNqqlLBuHA9SK7FVmPemg/viewform?embedded=true"
       title="Pick 'Ems Form"
       width="375"
       height="4190"
