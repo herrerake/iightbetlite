8fa826738ba006e193c6d32d89ce7511f3377855
Kevin Herrera <1herrerake@gmail.com>
2021-09-16 19:21:25 -0500

    week 2

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,13 +7,13 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 2 Sheet" />
     <h1>2021 NFL Week 2 Sheet</h1>
-    {/* <h3>Pot this week is $250</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQFfm-h-AZA7xh2NL-RtepT9oU6dxWnL4OsUD8eh81TKRq0HOtvpsz7yfkBsTFJf_UWAoJBbENTmeca/pubhtml?gid=63944394&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $475</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSJFUHJ22sth1anEnpRv9b67TMv94J-u4C_M9Mpe8pTetia66BvcRuQNQk2xnoBajOKxxXTqQBlnwqS/pubhtml?gid=1047598295&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
