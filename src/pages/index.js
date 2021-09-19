32e27f75f84dd8915ab298c4711abd22d83fa10c
Kevin Herrera <1herrerake@gmail.com>
2021-09-18 20:37:49 -0500

    cleaned wet code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import JSONHistory from "../../content/Pool-Winners-Data.json"
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 import { Container, Row, Col, Card, Button } from "react-bootstrap"
 
@@ -20,225 +19,36 @@ const IndexPage = () => (
       </Row>
       <Row>
         <Col>
-          <h2>History Feed</h2>
-          <Card>
-            <Card.Header>2021 NFL Week 1 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Thinh</Card.Title>
-              <Card.Text>Pot: $250</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/e/2PACX-1vQFfm-h-AZA7xh2NL-RtepT9oU6dxWnL4OsUD8eh81TKRq0HOtvpsz7yfkBsTFJf_UWAoJBbENTmeca/pubhtml?gid=63944394&single=true"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
+          <h1>History</h1>
         </Col>
       </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Wild Card Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: XxxDada89</Card.Title>
-              <Card.Text>Pot: $250</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1S3VP29MFdpHbeyf2qq_x1KlAEzKmqDvdP-IhMtF8BGs/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 15 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Sel</Card.Title>
-              <Card.Text>Pot: $100</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1XXTsrdgnQ2O3dWhcv1wEx3aLUbdbt3enWuJOPyh98dA/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 14 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Primo</Card.Title>
-              <Card.Text>Pot: $150</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1VCFIP-_X_0JxUhZt6X7KLkpZesreYGhQzQHKuVQKiwk/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 13 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Lachino</Card.Title>
-              <Card.Text>Pot: $250</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1UYttKDXN7Dk13yii5lvGEi3Kbq9hXPObAOOpeKBzyhc/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 11 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Split Salv & Primo</Card.Title>
-              <Card.Text>Pot: $200</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1OQnwPNnJPJhSrw-oV8mkgUVY3vgv7lvzPVFUwYR5xmw/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 10 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Primo</Card.Title>
-              <Card.Text>Pot: $325</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1A7_D5jbFqF76MMeyPXX4lJF7K7petWDvsaH378ycMAI/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 9 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Primo</Card.Title>
-              <Card.Text>Pot: $350</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1n0dmtNZCP9J9FVaFonTE0GRwQ5Cm73bLMPqLgAYB5Gk/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 8 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Salv</Card.Title>
-              <Card.Text>Pot: $300</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1kNwJ6b-WfKPS1wnP1o97llgA60UfPs9DjdZe5v-CPos/edit?usp=sharingx"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 7 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Chingateputo</Card.Title>
-              <Card.Text>Pot: $375</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1OtdWCQ0TpN4WSoBtlXTvm0WM0gRvqNfcFpi3z85JxTE/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row className="mt-5">
-        <Col>
-          <Card>
-            <Card.Header>2020 NFL Week 6 Pool (Paid)</Card.Header>
-            <Card.Body>
-              <Card.Title>Winner: Lou</Card.Title>
-              <Card.Text>Pot: $500</Card.Text>
-              <Button variant="primary">
-                <Link
-                  className="text-white"
-                  to="https://docs.google.com/spreadsheets/d/1KEWIyiP00jqze7rFaeD70nohUB7o9eWEQ4k1HaHrNrU/edit?usp=sharing"
-                  target="_blank"
-                >
-                  View Sheet
-                </Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      {JSONHistory.map((card, index) => {
+        return (
+          <Row className={index !== 0 ? "mt-5" : ""} key={card.uuid}>
+            <Col>
+              <Card>
+                <Card.Header>
+                  {card.season} NFL Week {card.week} Pool (Paid)
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>Winner: {card.winner}</Card.Title>
+                  <Card.Text>Pot: ${card.amount}</Card.Text>
+                  <Button variant="primary">
+                    <a
+                      className="text-white"
+                      href={card.link}
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      View Sheet
+                    </a>
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        )
+      })}
     </Container>
   </Layout>
 )
