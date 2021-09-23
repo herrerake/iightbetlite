bf8b0269c6228899c1e463b474c8a6cca273e728
Kevin Herrera <1herrerake@gmail.com>
2021-09-23 18:20:39 -0500

    week 3 sheet live

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,14 +7,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 3 Sheet" />
     <h1>2021 NFL Week 3 Sheet</h1>
-    {/* <h3>Pot this week is $475</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSJFUHJ22sth1anEnpRv9b67TMv94J-u4C_M9Mpe8pTetia66BvcRuQNQk2xnoBajOKxxXTqQBlnwqS/pubhtml?gid=1047598295&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $350 (+1300)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSi2r8ZmTsbE0piH4qBaPiXS80zfzXqNZJ13Fy-zCCu4fWEeudFp75sg--Dk90ypUrqZA1zIwAzza8t/pubhtml?gid=1593073966&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 2 Sheet"
-    ></iframe> */}
+      title="2021 NFL Week 3 Sheet"
+    ></iframe>
   </Layout>
 )
 
