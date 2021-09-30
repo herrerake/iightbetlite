4de1194a228c09256d8654fa1a7ed5eb47f6521c
Kevin Herrera <1herrerake@gmail.com>
2021-09-30 18:14:07 -0500

    week 4 ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,14 +7,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 4 Sheet" />
     <h1>2021 NFL Week 4 Sheet</h1>
-    {/* <h3>Pot this week is $350 (+1300)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSi2r8ZmTsbE0piH4qBaPiXS80zfzXqNZJ13Fy-zCCu4fWEeudFp75sg--Dk90ypUrqZA1zIwAzza8t/pubhtml?gid=1593073966&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $400 (+1500)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTcIO9VLhcd-wlnYH1hX_4QkofTDZCTVPznygO8o2l-RsBwanv-aWCqnFH-goBwIfxYcHclLukXsb1x/pubhtml?gid=1269930808&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 3 Sheet"
-    ></iframe> */}
+      title="2021 NFL Week 4 Sheet"
+    ></iframe>
   </Layout>
 )
 
