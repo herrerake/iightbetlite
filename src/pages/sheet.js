1b7c95e646cee16f11349fd1b6c9b8da056cfba0
Kevin Herrera <1herrerake@gmail.com>
2021-10-05 18:25:52 -0500

    update to end week 4

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -5,16 +5,16 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="2021 NFL Week 4 Sheet" />
-    <h1>2021 NFL Week 4 Sheet</h1>
-    <h3>Pot this week is $400 (+1500)</h3>
-    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
-    <iframe
+    <SEO title="2021 NFL Week 5 Sheet" />
+    <h1>2021 NFL Week 5 Sheet</h1>
+    {/* <h3>Pot this week is $400 (+1500)</h3> */}
+    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
+    {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTcIO9VLhcd-wlnYH1hX_4QkofTDZCTVPznygO8o2l-RsBwanv-aWCqnFH-goBwIfxYcHclLukXsb1x/pubhtml?gid=1269930808&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2021 NFL Week 4 Sheet"
-    ></iframe>
+    ></iframe> */}
   </Layout>
 )
 
