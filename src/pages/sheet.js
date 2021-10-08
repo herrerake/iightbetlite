7da05fcd6baabaca761fdf65de43c853a2aaf5e9
Kevin Herrera <1herrerake@gmail.com>
2021-10-07 19:58:35 -0500

    update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -7,7 +7,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 5 Sheet" />
     <h1>2021 NFL Week 5 Sheet</h1>
-    {/* <h3>Pot this week is $400 (+1500)</h3> */}
+    <h3>Pot this week is $325 (+1200)</h3>
     <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSb6GDEtgQ4ldfB_H3s5E9fY2aJkI_iaqwoGSnBE4sWNHkc4Rk2XluOrN7dhELKZ35CgSR3sBnoEifJ/pubhtml?gid=1698301608&amp;single=true&amp;widget=true&amp;headers=false"
