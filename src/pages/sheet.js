171fa3dcf7dd1c76f94d2d108296aa2a92e6f745
Kevin Herrera <1herrerake@gmail.com>
2021-10-14 23:11:44 -0500

    week 6 update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,7 +6,8 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 6 Sheet" />
     <h1>2021 NFL Week 6 Sheet</h1>
-    <h3>Pot this week is $475 (+1800)</h3>
+    <h3>Pot this week is $450 (+1700)</h3>
+    <p>One player did not pay and pool had to be adjusted from $475 to $450</p>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTnv7uSLaDs-GciO2gwSkJVG7bqDUiDkA8erKyfaX60niPicb1_174aQwBL-5tGVT5VNoK_Ui3kPZ9e/pubhtml?gid=1336089291&amp;single=true&amp;widget=true&amp;headers=false"
