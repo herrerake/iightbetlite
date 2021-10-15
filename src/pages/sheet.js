f8b693c83cb4bd7d8e93c3848a2fdcc2cd543c54
Kevin Herrera <1herrerake@gmail.com>
2021-10-14 23:48:27 -0500

    update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,8 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 6 Sheet" />
     <h1>2021 NFL Week 6 Sheet</h1>
-    <h3>Pot this week is $450 (+1700)</h3>
-    <p>One player did not pay and pool had to be adjusted from $475 to $450</p>
+    <h3>
+      Pot this week is{" "}
+      <span style={{ textDecoration: "line-through" }}>$475</span> $450 (+1700)
+    </h3>
+    <p>
+      Roy has been disqualified for not paying on time. Cannot join future pools
+      until he pays winner of week 6 $25
+    </p>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTnv7uSLaDs-GciO2gwSkJVG7bqDUiDkA8erKyfaX60niPicb1_174aQwBL-5tGVT5VNoK_Ui3kPZ9e/pubhtml?gid=1336089291&amp;single=true&amp;widget=true&amp;headers=false"
