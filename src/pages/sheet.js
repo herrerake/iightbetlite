ec49254a05a88921b7ae7bf9e8e2a4fcd51e228f
Kevin Herrera <1herrerake@gmail.com>
2021-10-14 19:23:39 -0500

    week 6 sheet

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 6 Sheet" />
     <h1>2021 NFL Week 6 Sheet</h1>
-    {/* <h3>Pot this week is $325 (+1200)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSb6GDEtgQ4ldfB_H3s5E9fY2aJkI_iaqwoGSnBE4sWNHkc4Rk2XluOrN7dhELKZ35CgSR3sBnoEifJ/pubhtml?gid=1698301608&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $475 (+1800)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTnv7uSLaDs-GciO2gwSkJVG7bqDUiDkA8erKyfaX60niPicb1_174aQwBL-5tGVT5VNoK_Ui3kPZ9e/pubhtml?gid=1336089291&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 5 Sheet"
-    ></iframe> */}
+      title="2021 NFL Week 6 Sheet"
+    ></iframe>
   </Layout>
 )
 
