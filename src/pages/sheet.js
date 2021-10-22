cf4ea0135c558144c7cd07d5969bfad77f763974
Kevin Herrera <1herrerake@gmail.com>
2021-10-21 20:21:59 -0500

    week 7 update

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,7 +6,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 7 Sheet" />
     <h1>2021 NFL Week 7 Sheet</h1>
-    <h3>Pot this week is $350 (+1300)</h3>
+    <h3>Pot this week is $375 (+1400)</h3>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTltS4hX7uPQ8p6JCSzQMvamrauT_5r_eTuy9kErZ65fJA4M5u4IfZTvFQ5fgf4Rsh7Ypj5bR3QrDsg/pubhtml?gid=2086087586&amp;single=true&amp;widget=true&amp;headers=false"
