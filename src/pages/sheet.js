9f7fb4ca5250643eff484b08637f2e2492f7eb16
Pat Tep <patonthestack@gmail.com>
2021-11-25 12:15:29 -0500

    NFL Week 12 Sheet Live

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 12 Sheet" />
     <h1>2021 NFL Week 12 Sheet</h1>
-    {/* <h3>Pot this week is $250 (+900)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSeOVVzqWTRvVB38yonCBxSlvzDbtg6-xVpZQ6fNTz3wgL2osRXLrJjcqlVOIr648gE6dBLviO894E8/pubhtml?gid=243043027&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $250 (+900)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTEMhTWxR6BpOTYuvANFJmXt4EWd_J2t_azmM1pKGyRJP-CGK_pfJ1eBPmMoapVyNdut5K1mnrZ-bbU/pubhtml?gid=1721121289&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 11 Sheet"
-    ></iframe> */}
+      title="2021 NFL Week 12 Sheet"
+    ></iframe>
   </Layout>
 )
 
