c61a2135c8ba276b09598c394e9558fa64cbc106
Kevin Herrera <1herrerake@gmail.com>
2021-12-07 13:23:19 -0600

    week 13 complete

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="2021 NFL Week 13 Sheet" />
-    <h1>2021 NFL Week 13 Sheet</h1>
-    <h3>Pot this week is $175 (+600)</h3>
-    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
-    <iframe
+    <SEO title="2021 NFL Week 14 Sheet" />
+    <h1>2021 NFL Week 14 Sheet</h1>
+    {/* <h3>Pot this week is $175 (+600)</h3> */}
+    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
+    {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTmwoJ6YKCnuFjNrcc4AMRHuwbh8MJw_HBvMVkXmIqEfMPS2oIYyG-VWSDI50gDSgXBgLqfqRQQauLO/pubhtml?gid=788360609&single=true"
       height="525"
       width="340"
       title="2021 NFL Week 13 Sheet"
-    ></iframe>
+    ></iframe> */}
   </Layout>
 )
 
