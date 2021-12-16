5af3e66d90103ac3d5f33efb1db66a2b31171bce
Kevin Herrera <1herrerake@gmail.com>
2021-12-16 12:43:23 -0600

    week 15 ready

--- src/pages/paid-version-form.js
+++ src/pages/paid-version-form.js
@@ -8,7 +8,7 @@ import { Container, Row, Col } from "react-bootstrap"
 
 const FormPage = () => (
   <Layout>
-    <SEO title="NFL Week 13 Form" />
+    <SEO title="NFL Week 15 Form" />
     <Container>
       <Row>
         <Col>
@@ -28,7 +28,7 @@ const FormPage = () => (
       </Row>
     </Container>
     <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfLJNdOkRf2V--FI1QOsy1IlPBZhvueGDHTNcjB0WLspMtfsg/viewform?embedded=true"
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeTl8X9fCb1rhTgBF66sQu6lB8mklwsgw_IXAgCNdlrKb6lyA/viewform?embedded=true"
       title="Pick 'Ems Form"
       width="375"
       height="4190"
--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="2021 NFL Week 14 Sheet" />
-    <h1>2021 NFL Week 14 Sheet</h1>
-    <h3>Pot this week is $175 (+600)</h3>
-    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
-    <iframe
+    <SEO title="2021 NFL Week 15 Sheet" />
+    <h1>2021 NFL Week 15 Sheet</h1>
+    {/* <h3>Pot this week is $175 (+600)</h3> */}
+    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
+    {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR4uWIOJbZ6jNhqPEIo6Be5EDFJl_G-qe2YbKJSL7Lmz0wD4h0tB1VZtwq5E0-_LO32bx_1M0s6wFRS/pubhtml?gid=895794928&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2021 NFL Week 14 Sheet"
-    ></iframe>
+    ></iframe> */}
   </Layout>
 )
 
