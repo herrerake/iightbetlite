05254a31bee1f370da1eb5b3208844a68981e72c
Kevin Herrera <1herrerake@gmail.com>
2022-01-08 16:26:00 -0600

    week 18 sheet

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 NFL Week 18 Sheet" />
     <h1>2021 NFL Week 18 Sheet</h1>
-    {/* <h3>Pot this week is $300 (+1100)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT2WwKiUSTGV5vteikxuVvS7GGQ9rEa9hCSMHWdyJps8n0xGYEqzbu_1hl_B6Vu4lxJaJ33yJzWSbje/pubhtml?gid=1518078720&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $175 (+800)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRd2tVhOdTq92g-RZGIKVgcdcx8ASXye0QSTfY-bi0mcGeKTxJr-gBA3s7iZzl6vZkxBzyCsccjAQss/pubhtml?gid=1060589350&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 17 Sheet"
-    ></iframe> */}
+      title="2021 NFL Week 18 Sheet"
+    ></iframe>
   </Layout>
 )
 
