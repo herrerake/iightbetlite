ce324067b46917daa6b95a69a9a9691285d9f3a8
Kevin Herrera <1herrerake@gmail.com>
2022-01-15 15:44:31 -0600

    sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 Season NFL Wild Card Sheet" />
     <h1>2021 Season NFL Wild Card Sheet</h1>
-    {/* <h3>Pot this week is $175 (+800)</h3> */}
-    <p>Check back 30min before the Saturday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRd2tVhOdTq92g-RZGIKVgcdcx8ASXye0QSTfY-bi0mcGeKTxJr-gBA3s7iZzl6vZkxBzyCsccjAQss/pubhtml?gid=1060589350&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $250 (+900)</h3>
+    {/* <p>Check back 30min before the Saturday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRAZPtoFtKO6mcdb7DJB8U6FBZX6JubF42zqP_zlW22Jux4fsNEmGMsraP_SDtUxQk94-TDgqGdJ8ED/pubhtml?gid=2039376387&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2021 NFL Week 18 Sheet"
-    ></iframe> */}
+      title="2021 NFL Wild Card Sheet"
+    ></iframe>
   </Layout>
 )
 
