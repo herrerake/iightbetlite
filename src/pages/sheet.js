73a35ab1c5c841f2b05f82db9860b33e8ce18e34
Kevin Herrera <1herrerake@gmail.com>
2022-01-22 16:23:30 -0600

    sheeet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2021 Season NFL Divisional Sheet" />
     <h1>2021 Season NFL Divisional Sheet</h1>
-    {/* <h3>Pot this week is $250 (+900)</h3> */}
-    <p>Check back 30min before the Saturday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRAZPtoFtKO6mcdb7DJB8U6FBZX6JubF42zqP_zlW22Jux4fsNEmGMsraP_SDtUxQk94-TDgqGdJ8ED/pubhtml?gid=2039376387&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $50 (+100)</h3>
+    {/* <p>Check back 30min before the Saturday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRs9KpiO4KH2yC2pRl6tU0g9Be3TLsEacDAnJ4493EK0JCw_21OTpv0F9bgQd5Z8ZSSd5gMkSeddS0H/pubhtml?gid=185132360&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2021 NFL Wild Card Sheet"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
