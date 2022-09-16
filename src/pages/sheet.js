90cfe6c30a27633a93380e254583cadac8101986
Kevin Herrera <1herrerake@gmail.com>
2022-09-15 19:22:22 -0500

    week 2 sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 2 Sheet" />
     <h1>2022 Season NFL Week 2 Sheet</h1>
-    {/* <h3>Pot this week is $250 (+900)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSKPN9irJwan4zz_sFu0dl_zGaBgUTXJkZqr1YB89Uq4ZGerfg1UkE429BUOXDBNMfq4kVHpjOC0H3y/pubhtml?gid=1269895067&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $300 (+1100)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS8sdlYu2jif6aYftp09SrynaYIY3zjINOESiGVQQGcPRLia5VuXp3LXhmvnDlJHYcv1BwxsUwAD9X4/pubhtml?gid=665732670&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
-      title="2022 NFL Season Week 1"
-    ></iframe> */}
+      title="2022 NFL Season Week 2"
+    ></iframe>
   </Layout>
 )
 
