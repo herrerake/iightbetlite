e35eca706a5af39b5c85195f40d2dda901143ca2
Pat Tep <patonthestack@gmail.com>
2022-09-22 19:34:43 -0400

    Week 3 Sheet Live

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 3 Sheet" />
     <h1>2022 Season NFL Week 3 Sheet</h1>
-    {/* <h3>Pot this week is $300 (+1100)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS8sdlYu2jif6aYftp09SrynaYIY3zjINOESiGVQQGcPRLia5VuXp3LXhmvnDlJHYcv1BwxsUwAD9X4/pubhtml?gid=665732670&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $225 (+800)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS8VFv5b8_jvHUUbDdI6pYgSB9wgalLwC7gNyxUY5VWDOtbLSDTwdpGPeAga6sZFLBZtViJJW54h-tV/pubhtml?gid=1076028554&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 3"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
