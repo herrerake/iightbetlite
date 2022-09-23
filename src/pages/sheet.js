a9314f0724ad16fa04bf31f320e7547d8085e888
Pat Tep <patonthestack@gmail.com>
2022-09-22 20:12:17 -0400

    Late add Henrock

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,7 +6,7 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 3 Sheet" />
     <h1>2022 Season NFL Week 3 Sheet</h1>
-    <h3>Pot this week is $250 (+900)</h3>
+    <h3>Pot this week is $275 (+1000)</h3>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS8VFv5b8_jvHUUbDdI6pYgSB9wgalLwC7gNyxUY5VWDOtbLSDTwdpGPeAga6sZFLBZtViJJW54h-tV/pubhtml?gid=1076028554&amp;single=true&amp;widget=true&amp;headers=false"
