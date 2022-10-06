28aac0b9261a01496b559f3dde1459d1afe71783
Pat Tep <patonthestack@gmail.com>
2022-10-06 17:46:31 -0400

    Pool winners, leaderboard update for Week 4

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="2022 Season NFL Week 4 Sheet" />
-    <h1>2022 Season NFL Week 4 Sheet</h1>
-    <h3>Pot this week is $250 (+900)</h3>
-    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
-    <iframe
+    <SEO title="2022 Season NFL Week 5 Sheet" />
+    {/* <h1>2022 Season NFL Week 4 Sheet</h1>
+    <h3>Pot this week is $250 (+900)</h3> */}
+    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
+    {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRHNaj1quBNQzHCiOCo0G4Ag2t6sNnIYZ9o-uh-oJkDWLU593PObQXprYJUVAk8O9s4wvDhgRJLqCVD/pubhtml?gid=1936741679&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 3"
-    ></iframe>
+    ></iframe> */}
   </Layout>
 )
 
