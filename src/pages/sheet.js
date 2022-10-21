e1b60afbbfe29a80f09bba6fa0012be229a52674
Kevin Herrera <1herrerake@gmail.com>
2022-10-20 19:23:32 -0500

    week 7 sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -5,15 +5,15 @@ import SEO from "../components/seo"
 const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 7 Sheet" />
-    {/* <h1>2022 Season NFL Week 7 Sheet</h1> */}
-    {/* <h3>Pot this week is $250 (+900)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRK_ZMQMSK-osGRP_fiLIqzY87WopPkudr_dU0fIRXMola97IDgmhXjLfG0qgxBiJavNQouflnVTHM_/pubhtml?gid=616133058&amp;single=true&amp;widget=true&amp;headers=false"
+    <h1>2022 Season NFL Week 7 Sheet</h1>
+    <h3>Pot this week is $150 (+500)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQu4NAgMNEppC_eQFzXqqfd6uLWbjkrJp-g7XorPh51HbwFYUtZHVUm3nWvKhlf6rb_OL7zijLL4LW1/pubhtml?gid=1242981610&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 7"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
