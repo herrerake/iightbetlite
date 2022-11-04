ade87aaa73c28be101f2bdd9804b499ada8b7ed1
Kevin Herrera <1herrerake@gmail.com>
2022-11-03 19:49:37 -0500

    sheet ready

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 9 Sheet" />
     <h1>2022 Season NFL Week 9 Sheet</h1>
-    {/* <h3>Pot this week is $225 (+800)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTtrrT7ml8WVXVwFjJsa-40YcQncQpsnQbmd2yo1IiKKpqCEa4L8avxeWTviOLAeLisguNyKuM52kVE/pubhtml?gid=1529623111&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $125 (+400)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQuyNrdWYSvven919YUbMy9ERUriPPi9gpJLfUysjQDSiz5Vbk8qnYlT55mwNSWidcRy6Ceq96rdd4v/pubhtml?gid=747782028&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 8"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
