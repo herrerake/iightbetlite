457152fcfdbadf6d257f53928f5d828f674a7715
Pat Tep <patonthestack@gmail.com>
2022-11-17 23:55:36 -0500

    correct src url

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -9,7 +9,7 @@ const SheetPage = () => (
     <h3>Pot this week is $125 (+400)</h3>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTs9UqF-HUW1LloWD7sVO3tA6jw8KKn0mAWr3p1GLYNAZEu4RN8Zs9srekl4sT19meuZexckeoWul4M/pubhtml?widget=true&amp;headers=false"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTs9UqF-HUW1LloWD7sVO3tA6jw8KKn0mAWr3p1GLYNAZEu4RN8Zs9srekl4sT19meuZexckeoWul4M/pubhtml?gid=1847550904&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 11"
