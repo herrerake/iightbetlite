5b50258fd967376376c208812c56d033e367b0e9
Kevin Herrera <1herrerake@gmail.com>
2022-11-24 16:13:29 -0500

    sheet ready 12

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -6,14 +6,14 @@ const SheetPage = () => (
   <Layout>
     <SEO title="2022 Season NFL Week 12 Sheet" />
     <h1>2022 Season NFL Week 12 Sheet</h1>
-    {/* <h3>Pot this week is $125 (+400)</h3> */}
-    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
-    {/* <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTs9UqF-HUW1LloWD7sVO3tA6jw8KKn0mAWr3p1GLYNAZEu4RN8Zs9srekl4sT19meuZexckeoWul4M/pubhtml?gid=1847550904&amp;single=true&amp;widget=true&amp;headers=false"
+    <h3>Pot this week is $175 (+600)</h3>
+    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS0JJs52WxLIi_rHBcX8J7KAAuEvlwhh11EqXHu9DoL3xD3xit5cu-5wmCdtdEpcyb_OmTgJ8oXNxDr/pubhtml?gid=1154618766&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 11"
-    ></iframe> */}
+    ></iframe>
   </Layout>
 )
 
