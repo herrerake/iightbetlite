c4f0216f7be587271a2fac8c91e342f2f76321da
Pat Tep <patonthestack@gmail.com>
2022-12-22 11:43:32 -0500

    Week 16 form ready, update leader board and pool winners

--- src/pages/paid-version-form.js
+++ src/pages/paid-version-form.js
@@ -2,18 +2,19 @@ import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/formLayout"
 import SEO from "../components/seo"
+
 import { Container, Row, Col } from "react-bootstrap"
 
 const FormPage = () => (
   <Layout>
-    <SEO title="2022 NFL Season Week 12 Form" />
+    <SEO title="2022 NFL Season Week 16 Form" />
     <Container>
       <Row>
         <Col>
           <h1>Weekly Pool</h1>
           <p>Winner take all</p>
           <p>Picks are due by Thursday @ 7PM EST</p>
-          <p>***Thanksgiving Week*** Picks are due by Thursday @ 11am EST</p>
+          {/* <p>***Thanksgiving Week*** Picks are due by Thursday @ 11am EST</p> */}
           <p>
             By submitting picks to this pool you agree to follow the rules laid
             out on <Link to="/rules">this page</Link>
@@ -26,7 +27,7 @@ const FormPage = () => (
       </Row>
     </Container>
     <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfaaZ28zLHvc9tNneGWSCrUA3AyzcL4f_Wl5Brd_mAZIvZIkg/viewform?embedded=true"
+      src="https://docs.google.com/forms/d/e/1FAIpQLScSKYP0ZBiU1rKcr2bzdvWy6SzEmDhTnDGo04W8oXwn0Hhj1w/viewform?embedded=true"
       title="Pick 'Ems Form"
       width="375"
       height="4190"
--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -4,16 +4,16 @@ import SEO from "../components/seo"
 
 const SheetPage = () => (
   <Layout>
-    <SEO title="2022 Season NFL Week 12 Sheet" />
-    <h1>2022 Season NFL Week 12 Sheet</h1>
-    <h3>Pot this week is $175 (+600)</h3>
-    {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
-    <iframe
+    <SEO title="2022 Season NFL Week 16 Sheet" />
+    <h1>2022 Season NFL Week 16 Sheet</h1>
+    {/* <h3>Pot this week is $175 (+600)</h3> */}
+    <p>Check back 30min before the Thursday kickoff game to see the sheet.</p>
+    {/* <iframe
       src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS0JJs52WxLIi_rHBcX8J7KAAuEvlwhh11EqXHu9DoL3xD3xit5cu-5wmCdtdEpcyb_OmTgJ8oXNxDr/pubhtml?gid=1154618766&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 11"
-    ></iframe>
+    ></iframe> */}
   </Layout>
 )
 
