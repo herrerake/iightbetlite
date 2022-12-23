83d3b8577f1a8b1b7b89e8e4e7ac8852632c84ab
Pat Tep <patonthestack@gmail.com>
2022-12-22 21:12:34 -0500

    Week 16 Sheet Live

--- src/pages/sheet.js
+++ src/pages/sheet.js
@@ -9,7 +9,7 @@ const SheetPage = () => (
     <h3>Pot this week is $75 (+200)</h3>
     {/* <p>Check back 30min before the Thursday kickoff game to see the sheet.</p> */}
     <iframe
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS0JJs52WxLIi_rHBcX8J7KAAuEvlwhh11EqXHu9DoL3xD3xit5cu-5wmCdtdEpcyb_OmTgJ8oXNxDr/pubhtml?gid=1154618766&amp;single=true&amp;widget=true&amp;headers=false"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTdu9yx33UNluQUsdEnoVZYm6504hXixJsgvqlkHo-oHptb3Dc9vMUriQpDWE86yjvfsX_ZGJNNKpwe/pubhtml?gid=1801405284&amp;single=true&amp;widget=true&amp;headers=false"
       height="525"
       width="340"
       title="2022 NFL Season Week 16"
